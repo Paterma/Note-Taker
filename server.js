09c7dc2870a1fc640270c7e4cc127f4365fe6241
Maria Paterno <mariapaterno7@yahoo.com>
2022-03-11 18:35:10 -0700

    fixed one error

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3001;
-const fs = require('fs');
+const fs = require('fs')
 const util = require('util');
-// const uuid = require('./helpers/uuid');
+const uuid = require('./helpers/uuid'); //generates unique ID's
 const path = require('path');
-const { json } = require('express/lib/response');
 const res = require('express/lib/response');
+const { parse } = require('path');
 const savedNotes = []; //array for my saved notes
 
 
@@ -21,10 +21,40 @@ app.use(express.static('public'));
 app.get('/', (req, res) =>
 res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+const readFileAsync = util.promisify(fs.readFile)
 // cnnecting with db.json
-app.get("/api/notes", function (req, res) {
-res.sendFile(path.join(__dirname, "db/db.json"))
-});
+app.get("/api/notes", (req, res) => {
+//   return res.sendFile(path.join(__dirname, "/db/db.json"))//THIS LINE WORKS
+
+// function read() {return readFileAsync('./db/db.json', 'utf8')}
+// function readFiles() {
+//     read().then(data => {
+//         let parsedData = [].concat(JSON.parse(data))
+//         console.log(parsedData)
+//         return parsedData
+//     })
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+        console.error(err);
+        } else {
+        //parsing data
+        let parsedData = [].concat(JSON.parse(data))
+       parsedData.push(content);
+       //   writeToFile(file, parsedData)
+//     //     }
+//     //     })} 
+// };
+// try {
+
+//     res.status(200).json(readFiles());
+//     console.log('success')
+// } catch {
+//     console.log(error)
+// }
+})
+
+//res.json()
 
 //route to notes specifically
 app.get("/notes", function (req, res) {
@@ -32,45 +62,39 @@ res.sendFile(path.join(__dirname, "public/notes.html"))
 });
 
 //fall back route
+// if nothing else matches then *
 app.get("/", (req, res) => {
 res.sendFile(path.join(__dirname, "index.html"));
 });
+// newNoteBtn = document.querySelector('.new-note');
 
+//destructing the notes
+// const { noteTitle, noteText} = req.body; //allows you to access data in a string (JSON) 
+// const notes = {
+//     noteTitle,
+//     noteText,
+//     review_id: uuid(),
+// };
 // //read the db file data, show an error if no data, push note if there is data
-fs.readFile('/db/db.json', (err, data) => {
-if (err) {
-console.error(err);
-} else {
-    const notes = req.body; //allows you to access data in a string (JSON)
-//pushing new notes in the saved array
-    savedNotes.push(notes);
-}
-})
-//parsing data
-const parsedData = data.toString()
-savedNotes = JSON.parse(parsedData)
-
-
-
 
 
 //making the string a file
-fs.writeFile(`./db/db.json`, JSON.stringify (savedNotes, (err) =>
-err
-? console.error(err)
-: console.log("New note has been saved")
-)
-);
-//a promise to run
-res.send("Achieved")
+// fs.writeFile(`./db/db.json`, JSON.stringify (savedNotes, (err) =>
+// err
+// ? console.error(err)
+// : console.log("New note has been saved")
+// )
+// );
+//need a param set up for the callback- returning the new note 
+// const specificTerm = req.params.somethinghere(toLowerCase){
+
+// }
 
 //the server will keep running
 app.listen(PORT, function () {
-    console.log(`Listening on ${PORT}...`)
+    console.log(`Listening on  http://localhost:${PORT}`)
 })
-
-
-
+//need to do a callback function
 //Link all proper folders/files
 //Do read and write files
 //create an empty array for notes to go
@@ -78,6 +102,9 @@ app.listen(PORT, function () {
 //Need to link db.json for the write file
 //app.get()
 //need a param set up for the callback- returning the new note 
-//need to go to heroku
+//DEPLOY TO HEROKU
 //The links are using get requests (clicking the button in index is firing off that get request, so the link)
 
+
+app.get("/api/notes", function (req, res) {
+    //   return res.sendFile(path.join(__dirname, "/db/db.json"))//THIS LINE WORKS
